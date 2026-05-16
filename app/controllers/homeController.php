c2160a4d5bf2193c8d3f7d3ea61240d80b3f731d
Đinh Hùng Quang <quangquang3434@gmail.com>
2026-05-16 22:02:51 +0700

    tạo url qua app

---
+++ app/controllers/homeController.php
@@ -0,0 +1,10 @@
+<?php
+class HomeController{
+    public function index(){
+        echo "index method from HomeController";
+    }
+
+    public function about(){
+        echo "about method from HomeController";
+    }
+}
---
+++ app/controllers/sinhvienController.php
@@ -0,0 +1,12 @@
+<?php
+class SinhvienController{
+    public function index(){
+        echo "index method from SinhvienController";
+    }
+
+    public function show($id){
+        echo "show method from SinhvienController" . $id;
+    }
+    
+}
+?>

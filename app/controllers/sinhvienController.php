b471675e31448099ff7d7eb28e637289e0098fd8
Đinh Hùng Quang <quangquang3434@gmail.com>
2026-06-08 20:07:52 +0700

    update controller and model sinhvien

--- app/controllers/sinhvienController.php
+++ app/controllers/sinhvienController.php
@@ -1,20 +1,47 @@
 <?php
-require_once '../app/core/controller.php';
-class SinhvienController extends Controller {
+require_once '../app/core/Controller.php';
+
+class SinhvienController extends Controller
+{
     public function index()
     {
         $sinhvienModel = $this->model('sinhvienModel');
-
         $sinhviens = $sinhvienModel->getAllSinhVien();
 
-        $this->view('sinhvien/index', [
-            'sinhviens' => $sinhviens
+        $this->view('layouts/masterlayouts', [
+            'title' => 'Danh sách sinh viên',
+            'view' => 'sinhvien/index',
+            'sinhviens' => $sinhviens,
         ]);
     }
 
     public function create()
     {
-        require_once '../app/views/sinhvien/create.php';
+        $this->view('layouts/masterlayouts', [
+            'title' => 'Thêm sinh viên',
+            'view' => 'sinhvien/create',
+        ]);
+    }
+
+    public function store()
+    {
+        if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+            header('Location: ?url=sinhvien/create');
+            exit();
+        }
+
+        $sinhvienModel = $this->model('sinhvienModel');
+        $sinhvienModel->insertSinhVien([
+            'ma_sv' => trim($_POST['ma_sv'] ?? ''),
+            'ho_ten' => trim($_POST['ho_ten'] ?? ''),
+            'gioi_tinh' => $_POST['gioi_tinh'] ?? '',
+            'ngay_sinh' => $_POST['ngay_sinh'] ?? null,
+            'dia_chi' => trim($_POST['dia_chi'] ?? ''),
+            'lop' => trim($_POST['lop'] ?? ''),
+        ]);
+
+        header('Location: ?url=sinhvien/index');
+        exit();
     }
 }
 ?>
--- app/models/sinhvienModel.php
+++ app/models/sinhvienModel.php
@@ -1,19 +1,37 @@
 <?php
-    require_once  '../app/core/ConnectDB.php';
+require_once '../app/core/ConnectDB.php';
 
-    class sinhvienModel {
-        private $conn;
+class sinhvienModel
+{
+    private $conn;
 
-        public function __construct() {
-           $this->conn = ConnectDB::connect();
-        }
-        public function getAllSinhVien() {
-            $query = "SELECT * FROM sinhvien";
-            $stmt = $this->conn->prepare($query);
-            $stmt->execute();
-            return $stmt->fetchAll(PDO::FETCH_ASSOC);
-        }
-        
+    public function __construct()
+    {
+        $this->conn = ConnectDB::connect();
     }
 
+    public function getAllSinhVien()
+    {
+        $query = 'SELECT * FROM sinhvien ORDER BY id DESC';
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function insertSinhVien($data)
+    {
+        $query = 'INSERT INTO sinhvien (ma_sv, ho_ten, gioi_tinh, ngay_sinh, dia_chi, lop) VALUES (:ma_sv, :ho_ten, :gioi_tinh, :ngay_sinh, :dia_chi, :lop)';
+        $stmt = $this->conn->prepare($query);
+
+        return $stmt->execute([
+            ':ma_sv' => $data['ma_sv'],
+            ':ho_ten' => $data['ho_ten'],
+            ':gioi_tinh' => $data['gioi_tinh'],
+            ':ngay_sinh' => $data['ngay_sinh'],
+            ':dia_chi' => $data['dia_chi'],
+            ':lop' => $data['lop'],
+        ]);
+    }
+}
 ?>
